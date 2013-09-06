a1276ea9f6ab9a015189ad67ad306d55658a8240
BertWang <stupid0912@gmail.com>
2013-09-07 02:17:10 +0800

    資料庫

--- website/application/views/report/list.php
+++ website/application/views/report/list.php
@@ -2,11 +2,11 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>includes/pagination/pagination.css" />
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>includes/templtes/01/images/css/search.css" />
 <header class="codrops-header">
-    <a href="/abroadplay/" title="公務員出國考察追蹤網-Home">
+    <a href="<?=base_url()?>" title="公務員出國考察追蹤網-Home">
         <h1>公務員出國考察追蹤網<span>追蹤公務員出國考察的、行程、人數</span></h1>
     </a>
 </header>
-<form class="form-wrapper cf" action="/abroadplay/report/search/" method="post">
+<form class="form-wrapper cf" action="<?=base_url()?>report/search/" method="post">
     <input type="text" placeholder="輸入查詢關鍵字..." name="key" value="<?=$key?>" required>
     <button type="submit">Search</button>
 </form>  
