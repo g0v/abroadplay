9331f58185d18c80d161f354f9bbb38330139b55
BertWang <stupid0912@gmail.com>
2013-11-22 00:02:04 +0800

    fix report list

--- website/application/views/report/list.php
+++ website/application/views/report/list.php
@@ -10,7 +10,7 @@
 <form  action="<?=base_url()?>report/search/" method="post">
 	<div class="form-datepicker cf">
 	<input type="text" id="from" name="Dfrom" value="<?=$Dfrom?>" placeholder="開始日" />
-	至
+	～
 	<input type="text" id="to" name="Dto" value="<?=$Dto?>" placeholder="結束日" />    
     </div>
     <div class="form-wrapper cf">
