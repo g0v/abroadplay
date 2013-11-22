83588061315e321f3b2edb769463c42426f0982f
BertWang <stupid0912@gmail.com>
2013-11-23 00:15:23 +0800

    add website redirect

---
+++ application/controllers/website.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Report extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->model('report_model');
+	}
+	public function index()
+	{
+		redirect(base_url().'report/lists/', 'refresh');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
