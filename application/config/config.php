0b7c33053d153117d4f1b936427c4f794c5b5a5d
BertWang <stupid0912@gmail.com>
2013-11-22 23:40:48 +0800

    fix

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://' . $_SERVER['HTTP_HOST'] . '/website';
+$config['base_url']	= 'http://' . $_SERVER['HTTP_HOST'] . '/';
 
 /*
 |--------------------------------------------------------------------------
