f87f9650f82769ff551ae55e85c7d7d9d8916212
Bert Wang <stupid0912@gmail.com>
2014-04-25 03:35:13 +0800

    add email contact

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
                                 </div>
                         </div><!-- /scroller-inner -->
-                        <div class="copyrights">2013  ©  公務員出國考察追蹤網  ||  本來源以 <a href="//report.nat.gov.tw/" target="_blank" title="公務出國報告資訊網">公務出國報告資訊網</a> 頒佈為準，資料僅供參考，若有不正確之處請來信與我們反應。 <br/><p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p></div> 
+                        <div class="copyrights">2013  ©  公務員出國考察追蹤網  ||  本來源以 <a href="//report.nat.gov.tw/" target="_blank" title="公務出國報告資訊網">公務出國報告資訊網</a> 頒佈為準，資料僅供參考，若有不正確之處請來信（<a href="mailto:abroadplay@groups.facebook.com">abroadplay@groups.facebook.com</a>）與我們反應。 <br/><p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p></div> 
                        
                 </div><!-- /scroller -->
                 
