2629aeec1f19581c32fbb31a20da2e6e6d73d82e
Bert Wang <stupid0912@gmail.com>
2014-06-06 11:20:48 +0800

    add Navigation

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    -->
     <meta name="Keywords" content="" />
     <meta name="Description" content="" />
     <meta property="og:title" content="<?php echo $title ?>" />
